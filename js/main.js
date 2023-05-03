23b34095fd8f0a40667b373abe382f2b07200236
thehoon8910 <thehoon8910@gmail.com>
2023-05-03 10:32:35 +0900

    기초 공부

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 var app = new Vue({
     el : "#app"
-})
+    , data : {
+        message : "Vue.js 시작!"
+        , list : ["리스트1", "리스트2", "리스트3"]
+        , show : true
+    }
+    , methods : {
+        handleClick:function(e){
+            alert(e.target);
+        }
+    }
+});
