1567599577c5d9248cc84df569dadc0ef10ee44d
thehoon8910 <thehoon8910@gmail.com>
2023-05-09 00:45:37 +0900

    데이터 등록과 변경 - 리스트 데이터 출력, 변경

--- ch2/js/main.js
+++ ch2/js/main.js
@@ -9,6 +9,37 @@ var app = new Vue({
         }
         // 배열 데이터
         , list : ["list1", "list2", "list3"]
+        , list2 : [
+            {id : 1, name:'A', hp : 100}
+            ,{id : 2, name:'B', hp : 300}
+            ,{id : 3, name:'C', hp : 500}
+        ]
         , num : 1
+        , scroll : 0
+        , count : 0
+        , isChild : true
+        , isActive : true
+        , textColor : '#ff0000'
+        , bgColor : '#00ff00'
+        , classObject : {
+            child:true
+            , 'is-active':false
+        }
+        , styleObject : {
+            color:'#ffff00'
+            , backgroundColor : '#00ffff'
+        }
+        , radius : 50
+        , ok : false
+        , type : 'C'
+    }
+    , mounted : function(){
+        this.scroll = 100 // 요소의 스크롤양 조작하기
+    }
+    , methods : {
+        // 버튼 클릭 시 이벤트 핸들러
+        increment : function(){
+            this.count+=1;
+        }
     }
 });
